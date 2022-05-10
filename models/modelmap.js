0ec4decb4388563c76b83b9aef47a51b4376d1b1
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-05-10 16:01:42 +0700

    Fix models, add samples

--- models/modelmap.js
+++ models/modelmap.js
@@ -19,9 +19,6 @@ class User extends Model {
 class BookProfile extends Model {
 }
 
-class PriceProfile extends Model {
-}
-
 class Book extends Model {
 }
 
@@ -50,54 +47,61 @@ class WildVoucher extends Model {
 Author.init({
     id: {
         type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true
     },
     name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
     },
     description: {
         type: DataTypes.TEXT,
         allowNull: true
     }
-}, {sequelize, modelName: 'author'});
+}, {sequelize, modelName: 'author', underscored: true});
 //endregion
 
 //region Publisher
 Publisher.init({
     id: {
         type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true
     },
     name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
     },
     description: {
         type: DataTypes.TEXT,
         allowNull: true
     }
-}, {sequelize, modelName: 'publisher'});
+}, {sequelize, modelName: 'publisher', underscored: true});
 //endregion
 
 //region Transporter
 Transporter.init({
     id: {
         type: DataTypes.INTEGER,
+        autoIncrement: true,
         primaryKey: true
     },
     name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
     },
     description: {
         type: DataTypes.TEXT,
         allowNull: true
     }
-}, {sequelize, modelName: 'transporter'});
+}, {sequelize, modelName: 'transporter', underscored: true});
 //endregion
 
 //region Transport
 Transport.init({
     id: {
         type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true
     },
     transporterid: {
@@ -108,9 +112,10 @@ Transport.init({
         }
     },
     transportid: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
     }
-}, {sequelize, modelName: 'transport'});
+}, {sequelize, modelName: 'transport', underscored: true});
 //endregion
 
 //region User
@@ -122,45 +127,62 @@ User.init({
     },
     firstname: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     lastname: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     phone: {
         type: DataTypes.STRING,
     },
     username: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     password: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     email: {
         type: DataTypes.STRING,
     },
     role: {
         type: DataTypes.ENUM('admin', 'user'),
+        defaultValue: 'user',
+        allowNull: false
     },
-    //TODO: fix database columns
     created_at: {
         type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
     },
     updated_at: {
         type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
     },
     is_active: {
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
     },
     is_verified: {
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
     },
     is_locked: {
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
     },
     is_blocked: {
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
     },
-}, {sequelize, paranoid: true, modelName: 'user'});
+}, {sequelize, paranoid: true, modelName: 'user', underscored: true});
 //endregion
 
 //region BookProfile
@@ -174,7 +196,8 @@ BookProfile.init({
         type: DataTypes.STRING
     },
     name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        allowNull: false
     },
     authorid: {
         type: DataTypes.INTEGER,
@@ -194,39 +217,21 @@ BookProfile.init({
         type: DataTypes.TEXT,
         allowNull: true
     },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
     ebookfile: {
         type: DataTypes.STRING
     }
-}, {sequelize, modelName: 'bookprofile'});
-//endregion
-
-//region PriceProfile
-PriceProfile.init({
-    bookprofileid: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        references: {
-            model: BookProfile,
-            key: 'id'
-        }
-    },
-    versionid: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    price: {
-        type: DataTypes.DECIMAL(10, 2)
-    },
-    edited: { //TODO: fix database column name
-        type: DataTypes.DATE
-    }
-}, {sequelize, modelName: 'priceprofile'});
+}, {sequelize, modelName: 'bookprofile', underscored: true});
 //endregion
 
 //region Book
 Book.init({
     id: {
         type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true
     },
     profileid: {
@@ -237,17 +242,20 @@ Book.init({
             key: 'id'
         }
     }
-}, {sequelize, modelName: 'book', paranoid: true, deletedAt: 'sell_at'});
+}, {sequelize, modelName: 'book', paranoid: true, deletedAt: 'sell_at', underscored: true});
 //endregion
 
 //region Bill
 Bill.init({
     id: {
         type: DataTypes.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true
     },
     date: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
     },
     transportid: {
         type: DataTypes.UUID,
@@ -264,9 +272,11 @@ Bill.init({
         }
     },
     billstatus: {
-        type: DataTypes.ENUM('pending', 'paid', 'cancelled')
+        type: DataTypes.ENUM('pending', 'paid', 'cancelled'),
+        allowNull: false,
+        defaultValue: 'pending'
     }
-}, {sequelize, modelName: 'bill'});
+}, {sequelize, modelName: 'bill', underscored: true});
 //endregion
 
 //region BillDetail
@@ -281,12 +291,17 @@ BillDetail.init({
     },
     bookid: {
         type: DataTypes.UUID,
+        primaryKey: true,
         references: {
             model: Book,
             key: 'id'
         }
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
     }
-}, {sequelize, modelName: 'billdetail'});
+}, {sequelize, modelName: 'billdetail', underscored: true});
 //endregion
 
 //region CartItem
@@ -309,20 +324,22 @@ CartItem.init({
     },
     selected: {
         type: DataTypes.BOOLEAN,
+        allowNull: false,
         defaultValue: true
     }
-}, {sequelize, modelName: 'cart'});
+}, {sequelize, modelName: 'cart', underscored: true});
 //endregion
 
 //region Lend
 Lend.init({
     id: {
         type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
     },
     userid: {
         type: DataTypes.UUID,
-        primaryKey: true,
+        allowNull: false,
         references: {
             model: User,
             key: 'id'
@@ -330,19 +347,21 @@ Lend.init({
     },
     bookprofileid: {
         type: DataTypes.UUID,
-        primaryKey: true,
+        allowNull: false,
         references: {
             model: BookProfile,
             key: 'id'
         }
     },
     start: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false
     },
     end: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false
     }
-}, {sequelize, modelName: 'lend'});
+}, {sequelize, modelName: 'lend', underscored: true});
 //endregion
 
 //region VoucherProfile
@@ -353,17 +372,23 @@ VoucherProfile.init({
         primaryKey: true
     },
     //TODO: add more fields
-}, {sequelize, modelName: 'voucherprofile'});
+}, {sequelize, modelName: 'voucherprofile', underscored: true});
 //endregion
 
 //region Voucher
 Voucher.init({
     id: {
         type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true
     },
     userid: {
         type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
     },
     profileid: {
         type: DataTypes.UUID,
@@ -375,6 +400,7 @@ Voucher.init({
     //TODO: add more fields
     vouchercode: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     expireddate: {
         type: DataTypes.DATE,
@@ -383,7 +409,7 @@ Voucher.init({
     useddate: {
         type: DataTypes.DATE,
     }
-}, {sequelize, modelName: 'voucher'});
+}, {sequelize, modelName: 'voucher', underscored: true});
 //endregion
 
 //region WildVoucher
@@ -413,65 +439,229 @@ WildVoucher.init({
     used: {
         type: DataTypes.BLOB,
     }
-}, {sequelize, modelName: 'wildvoucher'});
+}, {sequelize, modelName: 'wildvoucher', underscored: true});
 //endregion
 
 Author.hasMany(BookProfile, {foreignKey: 'authorid'});
+BookProfile.belongsTo(Author, {foreignKey: 'authorid'});
 
 Book.belongsTo(BookProfile, { foreignKey: 'profileid' });
+BookProfile.hasMany(Book, { foreignKey: 'profileid' });
 
-BookProfile.belongsTo(Author, {foreignKey: 'authorid'});
 BookProfile.belongsTo(Publisher, {foreignKey: 'publisherid'});
-BookProfile.hasOne(PriceProfile, {foreignKey: 'bookprofileid'});
-BookProfile.hasMany(Book, {foreignKey: 'profileid'});
-BookProfile.hasMany(Lend, {foreignKey: 'bookprofileid'});
-/*BookProfile.belongsToMany(User, {through: 'lend', foreignKey: 'bookprofile'});
-BookProfile.belongsToMany(User, {through: 'cartitem', foreignKey: 'bookprofile'});*/
-
-Bill.belongsTo(User, {foreignKey: 'userid'});
-Bill.belongsTo(Transport, {foreignKey: 'transportid'});
-Bill.hasMany(BillDetail, {foreignKey: 'billid'});
-
-BillDetail.belongsTo(Bill, {foreignKey: 'billid'});
-BillDetail.belongsTo(Book, {foreignKey: 'bookid'});
-
-/*CartItem.belongsTo(BookProfile, {foreignKey: 'bookprofile'});
-CartItem.belongsTo(User, {foreignKey: 'userid'});
-
-Lend.belongsTo(User, {foreignKey: 'userid'});
-Lend.belongsTo(BookProfile, {foreignKey: 'bookprofileid'});*/
-
-PriceProfile.belongsTo(BookProfile, {foreignKey: 'bookprofileid'});
-
 Publisher.hasMany(BookProfile, {foreignKey: 'publisherid'});
 
-Transport.belongsTo(Transporter, {foreignKey: 'transporterid'});
+BookProfile.hasMany(Lend, {foreignKey: 'bookprofileid'});
+Lend.belongsTo(BookProfile, {foreignKey: 'bookprofileid'});
+
+BookProfile.belongsToMany(User, {through: Lend, foreignKey: 'bookprofile'});
+User.belongsToMany(BookProfile, {through: Lend, foreignKey: 'userid'});
+
+BookProfile.belongsToMany(User, {through: CartItem, foreignKey: 'bookprofile'});
+User.belongsToMany(BookProfile, {through: CartItem, foreignKey: 'userid'});
+
+Bill.belongsTo(User, {foreignKey: 'userid'});
+User.hasMany(Bill, {foreignKey: 'userid'});
+
+Bill.belongsTo(Transport, {foreignKey: 'transportid'});
 Transport.hasOne(Bill, {foreignKey: 'transportid'});
 
+Bill.hasMany(BillDetail, {foreignKey: 'billid'});
+BillDetail.belongsTo(Bill, {foreignKey: 'billid'});
+
+BillDetail.belongsTo(Book, {foreignKey: 'bookid'});
+Book.hasOne(BillDetail, {foreignKey: 'bookid'});
+
+Transport.belongsTo(Transporter, {foreignKey: 'transporterid'});
 Transporter.hasMany(Transport, {foreignKey: 'transporterid'});
 
 User.hasMany(Voucher, {foreignKey: 'userid'});
-User.hasMany(Bill, {foreignKey: 'userid'});
+Voucher.belongsTo(User, {foreignKey: 'userid'})
+
 User.hasMany(Lend, {foreignKey: 'userid'});
+Lend.belongsTo(User, {foreignKey: 'userid'});
+
 User.hasMany(CartItem, {foreignKey: 'userid'});
-/*User.belongsToMany(BookProfile, {through: CartItem, foreignKey: 'userid'});
-User.belongsToMany(BookProfile, {through: Lend, foreignKey: 'userid'});*/
+CartItem.belongsTo(User, {foreignKey: 'userid'});
 
 Voucher.belongsTo(VoucherProfile, {foreignKey: 'profileid'});
-
 VoucherProfile.hasMany(Voucher, {foreignKey: 'profileid'});
-VoucherProfile.hasMany(WildVoucher, {foreignKey: 'profileid'});
 
+VoucherProfile.hasMany(WildVoucher, {foreignKey: 'profileid'});
 WildVoucher.belongsTo(VoucherProfile, {foreignKey: 'profileid'});
+
+async function createSampleData() {
+    const author1 = await Author.create({
+        name: 'J.K. Rowling',
+    });
+    const author2 = await Author.create({
+        name: 'J.R.R. Tolkien',
+    });
+    const author3 = await Author.create({
+        name: 'Stephen King',
+    });
+
+    const publisher1 = await Publisher.create({
+        name: 'Harper & Brothers',
+    });
+    const publisher2 = await Publisher.create({
+        name: 'Simon & Schuster',
+    });
+    const publisher3 = await Publisher.create({
+        name: 'Penguin',
+    });
+
+    const transporter1 = await Transporter.create({
+        name: 'Shopee',
+    });
+    const transporter2 = await Transporter.create({
+        name: 'Tiki',
+    });
+    const transporter3 = await Transporter.create({
+        name: 'Giaohangtietkiem',
+    });
+
+    const transport1 = await Transport.create({
+        transporterid: transporter1.id,
+        transportid: "abc123"
+    });
+    const transport2 = await Transport.create({
+        transporterid: transporter2.id,
+        transportid: "abc124"
+    });
+    const transport3 = await Transport.create({
+        transporterid: transporter3.id,
+        transportid: "abc125"
+    });
+
+    const user1 = await User.create({
+        firstname: 'John',
+        lastname: 'Doe',
+        username: 'admin',
+        password: 'admin',
+        role: 'admin',
+    });
+    const user2 = await User.create({
+        firstname: 'Jane',
+        lastname: 'Doe',
+        username: 'user',
+        password: 'user',
+        role: 'user',
+    });
+    const user3 = await User.create({
+        firstname: 'Jack',
+        lastname: 'Doe',
+        username: 'guest',
+        password: 'guest',
+        role: 'user'
+    });
+
+    const bookProfile1 = await BookProfile.create({
+        name: 'Harry Potter and the Sorcerers Stone',
+        publisherid: publisher1.id,
+        authorid: author1.id,
+        price: 100
+    });
+    const bookProfile2 = await BookProfile.create({
+        name: 'The Hobbit',
+        publisherid: publisher2.id,
+        authorid: author2.id,
+        price: 200
+    });
+    const bookProfile3 = await BookProfile.create({
+        name: 'The Shining',
+        publisherid: publisher3.id,
+        authorid: author3.id,
+        price: 300
+    });
+
+    const book1 = await Book.create({
+        profileid: bookProfile1.id
+    });
+    const book2 = await Book.create({
+        profileid: bookProfile2.id
+    });
+    const book3 = await Book.create({
+        profileid: bookProfile3.id
+    });
+
+    const bill1 = await Bill.create({
+        userid: user1.id,
+        transportid: transport1.id,
+        billstatus: 'pending'
+    });
+    const bill2 = await Bill.create({
+        userid: user2.id,
+        transportid: transport2.id,
+        billstatus: 'pending'
+    });
+
+    const billDetail1 = await BillDetail.create({
+        billid: bill1.id,
+        bookid: book1.id,
+        price: bookProfile1.price
+    });
+    const billDetail2 = await BillDetail.create({
+        billid: bill1.id,
+        bookid: book2.id,
+        price: bookProfile2.price
+    });
+    const billDetail3 = await BillDetail.create({
+        billid: bill2.id,
+        bookid: book3.id,
+        price: bookProfile3.price
+    });
+
+    const cartItem1 = await CartItem.create({
+        userid: user3.id,
+        bookprofile: bookProfile1.id,
+    });
+    const cartItem2 = await CartItem.create({
+        userid: user3.id,
+        bookprofile: bookProfile2.id,
+    });
+    const cartItem3 = await CartItem.create({
+        userid: user1.id,
+        bookprofile: bookProfile3.id,
+    });
+
+    const lend1 = await Lend.create({
+        userid: user1.id,
+        bookprofileid: bookProfile1.id,
+        start: new Date(),
+        end: new Date(),
+    });
+
+    const voucherProfile1 = await VoucherProfile.create({
+
+    });
+
+    const voucher1 = await Voucher.create({
+        profileid: voucherProfile1.id,
+        userid: user1.id,
+        vouchercode: '12345',
+    });
+}
 
 async function Init() {
     await tryConnect();
-    sequelize.sync().then(r => {
+    await sequelize.sync({ force: false }).then(r => {
         console.log('Database connected');
     })
+    // await createSampleData();
+    // Find all users
+    const users = await User.findAll({
+        include: [{
+            model: Bill
+        }]
+    });
+    console.log(users.every(user => user instanceof User));
+    console.log("All users:", JSON.stringify(users, null, 2));
 }
 
-Init();
+Init().then(async () => {
+    console.log('Database created');
+});
 
 module.exports = {
     Author,
@@ -481,13 +671,11 @@ module.exports = {
     BillDetail,
     CartItem,
     Lend,
-    PriceProfile,
     Publisher,
     Transport,
     Transporter,
     User,
     Voucher,
     VoucherProfile,
-    WildVoucher,
-    Init
+    WildVoucher
 }
