d2642a6e0202e73afa534e6775e08cf0d8897113
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-05-17 02:20:33 +0700

    .

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const logger = require('morgan');
 
 const modelmap = require('./models/modelmap');
 
-const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api/api');
 
 const app = express();
@@ -18,23 +17,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
 });
 
 app.listen(process.env.PORT, () => {
