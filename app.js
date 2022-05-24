4538b6582a43efb5dcd52b1f10cd121b7346838a
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-05-24 19:53:01 +0700

    .

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: true}});
 
 const createError = require('http-errors');
 const express = require('express');
@@ -12,14 +13,27 @@ const modelmap = require('./models/modelmap');
 const apiRouter = require('./routes/api/api');
 const authRouter = require('./routes/auth/auth');
 
+// Imports the Google Cloud client library
+const {ErrorReporting} = require('@google-cloud/error-reporting');
+
+// Instantiates a client
+const errors = new ErrorReporting();
+
 const app = express();
+
+app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use('/bodytest', (req, res, next) => {
+  return res.json(req.body).end();
+});
+
 app.use(logger('dev'));
 
-app.use('/api', apiRouter);
 app.use(authRouter);
+app.use('/api', apiRouter);
 
 app.use(passport.initialize());
 
@@ -27,12 +41,20 @@ app.use((req, res, next) => {
     next(createError(404));
 });
 
+app.use(errors.express);
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     res.status(err.status || 500);
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the API',
+    status: 'OK'
+  });
 });
 
 app.listen(envVariables.PORT, '0.0.0.0', () => {
--- config/database.js
+++ config/database.js
@@ -1,25 +1,44 @@
-const {Sequelize} = require("sequelize");
+const {Sequelize} = require('sequelize');
 const dbVariable = require('../variables/database');
 
-const sequelize = new Sequelize({
-    dialect: dbVariable.DATABASE_DRIVER,
-    host: dbVariable.DATABASE_HOST,
-    port: dbVariable.DATABASE_PORT,
-    username: dbVariable.DATABASE_USERNAME,
-    password: dbVariable.DATABASE_PASSWORD,
-    database: dbVariable.DATABASE_NAME,
-})
+const createConnection = () => {
+  if (!dbVariable.DATABASE_SOCKET_PATH) {
+    return new Sequelize({
+      dialect: dbVariable.DATABASE_DRIVER,
+      host: dbVariable.DATABASE_HOST,
+      port: dbVariable.DATABASE_PORT,
+      username: dbVariable.DATABASE_USERNAME,
+      password: dbVariable.DATABASE_PASSWORD,
+      database: dbVariable.DATABASE_NAME,
+    });
+  } else {
+    const connectionString = `${dbVariable.DATABASE_SOCKET_PATH}/${dbVariable.DATABASE_CONNECTION_NAME}`;
+    return new Sequelize({
+      dialect: dbVariable.DATABASE_DRIVER,
+      host: connectionString,
+      port: dbVariable.DATABASE_PORT,
+      username: dbVariable.DATABASE_USERNAME,
+      password: dbVariable.DATABASE_PASSWORD,
+      database: dbVariable.DATABASE_NAME,
+      dialectOptions: {
+        socketPath: connectionString
+      }
+    });
+  }
+};
 
-async function tryConnect() {
+const sequelize = createConnection();
+
+  async function tryConnect() {
     try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+      console.error('Unable to connect to the database:', error);
     }
-}
+  };
 
 module.exports = {
-    sequelize,
-    tryConnect
+  sequelize,
+  tryConnect,
 };
