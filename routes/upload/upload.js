1611b5cc43514ec1e36449251d7e1da95eacf8da
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-06-09 01:25:18 +0700

    .

--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -3,16 +3,29 @@ const authMiddlewares = require('../auth/auth.middleware');
 const router = require('express').Router();
 
 router.post('/upload/image'//, authMiddlewares.verifyToken, authMiddlewares.verifyRole('admin')
-  , middleware.upload.single('image'), async (req, res) => {
-    const fileUpload = middleware.folder;
+    , middleware.upload.single('image'), async (req, res) => {
+        const fileUpload = middleware.imageSaver;
 
-    if (!req.file) {
-      res.status(401).json({error: 'Please provide an image'});
-    }
+        if (!req.file) {
+            res.status(401).json({error: 'Please provide an image'});
+        }
 
-    const fileId = await fileUpload.save(req.file.buffer);
+        const file = await fileUpload.save(req.file);
 
-    return res.status(200).json({ name: fileId });
-});
+        return res.status(200).json(file);
+    });
+
+router.post('/upload/video'//, authMiddlewares.verifyToken, authMiddlewares.verifyRole('admin')
+    , middleware.upload.single('video'), async (req, res) => {
+        const fileUpload = middleware.videoSaver;
+
+        if (!req.file) {
+            res.status(401).json({error: 'Please provide an video'});
+        }
+
+        const file = await fileUpload.save(req.file);
+
+        return res.status(200).json(file);
+    });
 
 module.exports = router;
--- routes/upload/upload.middleware.js
+++ routes/upload/upload.middleware.js
@@ -2,43 +2,100 @@ const multer = require('multer');
 const appVariable = require('../../variables/app.variable');
 const path = require('path');
 const sharp = require('sharp');
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const fs = require('fs');
+const {ImageRepository, VideoRepository} = require("../../repositories/file.repository");
+const getVideoDurationInSeconds = require("get-video-duration");
+const {Readable} = require("stream");
 
 const upload = multer({
-  limits: {
-    fileSize: 4 * 1024 * 1024,
-  },
+    limits: {
+        fileSize: 4 * 1024 * 1024,
+    },
 });
 
-class Resize {
+class ImageSaver {
 
-  constructor(folder) {
-    this.folder = folder;
-  }
+    constructor(folder) {
+        this.folder = folder;
+    }
 
-  async save(buffer) {
-    const filename = Resize.filename();
-    const filepath = this.filepath(filename);
+    /**
+     *
+     * @param {Multer.File} file
+     * @returns {Promise<Image>}
+     */
+    async save(file) {
+        const fileId = uuidv4();
+        const filename = ImageSaver.filename(fileId);
+        const filepath = this.filepath(filename);
 
-    await fs.promises.mkdir(this.folder, { recursive: true })
+        await fs.promises.mkdir(this.folder, {recursive: true})
 
-    await sharp(buffer)
-      .toFile(filepath);
+        const output = await sharp(file.buffer)
+            .toFile(filepath);
 
-    return filename;
-  }
+        const image = ImageRepository.create({
+            id: fileId,
+            path: filename,
+            name: file.originalname,
+            width: output.width,
+            height: output.height,
+        });
+        return await ImageRepository.save(image);
+    }
 
-  static filename() {
-    return `${uuidv4()}.png`;
-  }
+    static filename(uuid) {
+        return `${uuid}.png`;
+    }
 
-  filepath(filename) {
-    return path.resolve(`${this.folder}/${filename}`)
-  }
+    filepath(filename) {
+        return path.resolve(`${this.folder}/${filename}`)
+    }
 
 }
 
-const folder = new Resize(path.join(process.cwd(), appVariable.UPLOAD_FOLDER));
+class VideoSaver {
 
-module.exports = { upload, folder }
+    constructor(folder) {
+        this.folder = folder;
+    }
+
+    /**
+     *
+     * @param {Multer.File} file
+     * @returns {Promise<Video>}
+     */
+    async save(file) {
+        const fileId = uuidv4();
+        const filename = VideoSaver.filename(fileId, (/[.]/.exec(file.originalname)) ? /[^.]+$/.exec(file.originalname) : undefined);
+        const filepath = this.filepath(filename);
+
+        await fs.promises.mkdir(this.folder, {recursive: true})
+
+        const stream = Readable.from(file.buffer);
+        const output = await getVideoDurationInSeconds(stream);
+
+        const video = VideoRepository.create({
+            id: fileId,
+            path: filename,
+            name: file.originalname,
+            duration: output,
+        });
+        return await VideoRepository.save(video);
+    }
+
+    static filename(uuid, extension) {
+        return `${uuid}.${extension}`;
+    }
+
+    filepath(filename) {
+        return path.resolve(`${this.folder}/${filename}`)
+    }
+
+}
+
+const imageSaver = new ImageSaver(path.join(process.cwd(), appVariable.UPLOAD_IMAGE_FOLDER));
+const videoSaver = new VideoSaver(path.join(process.cwd(), appVariable.UPLOAD_VIDEO_FOLDER));
+
+module.exports = {upload, imageSaver, videoSaver}
