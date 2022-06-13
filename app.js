7f9f1bbf5ed05c6baac9b40fefc5a0e7dacc0475
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-06-13 11:10:48 +0700

    .

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const swaggerDocs = require("./utils/swagger");
 const RunApp = async () => {
     const app = express();
 
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
+
     app.set('trust proxy', true);
     app.use(express.static('public'));
 
